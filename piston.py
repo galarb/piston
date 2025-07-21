6e07a34d62ac6c1e6da4fbccf24a32c873507b93
gal arbel <84223483+galarb@users.noreply.github.com>
2025-07-21 13:44:54 +0300

    Update piston.py

--- piston.py
+++ piston.py
@@ -17,10 +17,14 @@ class Piston:
         self.pwm2.duty(1023)
         
     def motgo(self, speed):
+
         pwm_value = int(min(max(abs(speed), 0), 100) * 10.23)  # Map -100 to 100 to 0 to 1023
+        pwm_value = max(0, min(pwm_value, 1023))
+
         #print('speed pwm value = ', pwm_value)
         if speed > 0:
             # Forward direction
+            print("Motor forward at", pwm_value)
             self.pwm1.duty(pwm_value)
             self.pwm2.duty(0)
         elif speed < 0:
@@ -29,5 +33,14 @@ class Piston:
             self.pwm2.duty(pwm_value)
         else:
             # Stop the motor
-            self.pwm1.duty(0)
-            self.pwm2.duty(0)    
+                self.stop()
+
+            
+    def stop(self):
+        self.pwm1.duty(0)
+        self.pwm2.duty(0)
+        
+    def demo(self):
+        for spd in [50, -50, 0]:
+            self.motgo(spd)
+            time.sleep(1)
